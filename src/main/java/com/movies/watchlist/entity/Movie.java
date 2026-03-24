415ab569b3910f6420ce893c87c99330a3b4ab17
Bhaumik-05 <modibhaumik180@gmail.com>
2026-03-24 14:28:10 +0530

    Initial commit - Spring Boot project with User and Movie entities

---
+++ src/main/java/com/movies/watchlist/entity/Movie.java
@@ -0,0 +1,25 @@
+package com.movies.watchlist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "movies")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String genre;
+
+    private Integer releaseYear;
+}
---
+++ src/main/java/com/movies/watchlist/entity/User.java
@@ -0,0 +1,27 @@
+package com.movies.watchlist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
