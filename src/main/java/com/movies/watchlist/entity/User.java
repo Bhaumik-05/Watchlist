566aa09bf83a6fc93b5627a4f880eddfa983e517
Bhaumik-05 <modibhaumik180@gmail.com>
2026-03-25 15:20:24 +0530

    [FIX] minor import fix

--- src/main/java/com/movies/watchlist/entity/User.java
+++ src/main/java/com/movies/watchlist/entity/User.java
@@ -3,7 +3,6 @@ package com.movies.watchlist.entity;
 import com.movies.watchlist.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
